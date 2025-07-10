15519950394737bb27c28643bc9008337b53646d
Steven AKOTI <steven.akoti@gmail.com>
2025-07-10 09:37:07 +0200

    Stockage et interrogation des données avec DuckDB

--- projet_streamlit.py
+++ projet_streamlit.py
@@ -1,4 +1,4 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 
 st.title("Application Web")
@@ -7,12 +7,35 @@ st.write("Bienvenue sur notre application web interactive")
 
 st.subheader("Téléversement de fichier CSV")
 
-fichier_csv = st.file_uploader("Importez votre fichier CSV", type=["csv"])
+fichier_csv = st.file_uploader("Importez votre fichier CSV", type=["csv"])    #Téléversement de fichier CSV
 
 if fichier_csv is not None:
     df = pd.read_csv(fichier_csv)
     st.write("Aperçu des données :")
-    st.dataframe(df)
+    st.dataframe(df)                                                          #############################
+   
+import duckdb
+
+# Connexion à une base DuckDB en mémoire (temporaire)
+connexion = duckdb.connect(database=':memory:')
+
+# Création d'une table "amazon_prime_titles" avec les données du DataFrame pandas
+connexion.execute("CREATE TABLE amazon_prime_titles AS SELECT * FROM df")
+
+# Interrogation des données
+
+query = """
+SELECT *
+FROM amazon_prime_titles
+"""
+
+result_df = connexion.execute(query).df()   # Résultat sous forme de DataFrame pandas
+st.dataframe(result_df)                     # Affichage du résultat
+
+
+
+
+
 
 
 
